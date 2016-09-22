33804ac50ac6c27723c2d4f0a0aee16d2d0aa736
Daniel Cardenas <dan85.cardenas@gmail.com>
2016-09-21 20:55:41 -0400

    Add AWS for Paperclip uploads to Heroku

--- Gemfile
+++ Gemfile
@@ -58,3 +58,4 @@ end
 gem 'devise', '~> 4.2'
 
 gem 'paperclip', '~> 5.1'
+gem 'aws-sdk', '~> 2.3', '>= 2.3.19'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,15 @@ Rails.application.configure do
   # Paperclip config:
   Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
   Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
+
+  #AWS Config. Sets images to upload with paperclip:
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
 end
