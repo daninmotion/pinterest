6b987268d7619c5b85442392c8871f1bab5226b1
Daniel Cardenas <dan85.cardenas@gmail.com>
2016-09-26 19:54:12 -0400

    application.js config

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require jquery.turbolinks
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require turbolinks-compatibility
 //= require masonry/jquery.masonry
